Furkanzmc/firvish.nvim

commit 5cbe8a72c38fc93963d662451fd45e87f93a33e9

feat: open quickfix/loc list on_exit

lua/firvish/job_control.lua
@@ -53,12 +53,12 @@ local function create_job_list_item(job_id, job)
     line = line .. ' ' .. cmdString
 
     -- These are the optional line that will be echoed on cursor hold.
-    local additonal = 'Started: "' .. job.start_time .. '"'
+    local additional = 'Started: "' .. job.start_time .. '"'
     if not job.running then
-        additonal = additonal .. ' Finished: "' .. job.finish_time .. '"'
+        additional = additional .. ' Finished: "' .. job.finish_time .. '"'
     end
 
-    return {echo = additonal, job_id = job_id, line = line}
+    return {echo = additional, job_id = job_id, line = line}
 end
 
 local function create_job_list_window(lines, job_list)
@@ -73,7 +73,7 @@ local function get_jobs_preview_data()
     local job_list = {}
 
     for job_id, value in pairs(s_jobs) do
-        line = create_job_list_item(job_id, value)
+        local line = create_job_list_item(job_id, value)
 
         table.insert(job_list, line)
     end
@@ -104,6 +104,8 @@ local function check_start_job_args(opts)
     elseif opts.output_qf == 0 then
         opts.output_qf = false
     end
+    opts.open_qf = opts.output_qf and opts.open_qf or false
+    opts.open_lqf = opts.output_lqf and opts.open_lqf or false
 
     opts.cwd = opts.cwd or fn.getcwd()
     opts.cwd = fn.expand(opts.cwd)
@@ -286,6 +288,12 @@ local function on_exit(job_id, exit_code, event)
     else
         s_jobs[job_id] = nil
     end
+
+    if job_info.open_qf then
+      cmd [[copen]]
+    elseif job_info.open_lqf then
+      cmd [[lopen]]
+    end
 end
 
 -- }}}
@@ -345,25 +353,18 @@ M.start_job = function(opts)
         api.nvim_buf_set_var(bufnr, "firvish_job_id", job_id)
     end
 
-    s_jobs[job_id] = {
-        bufnr = bufnr,
-        cmd = opts.cmd,
-        title = buf_title,
-        stdout = {},
-        stderr = {},
-        output = {},
-        running = true,
-        is_background_job = opts.is_background_job,
-        start_time = fn.strftime('%H:%M:%S'),
-        finish_time = "",
-        exit_code = nil,
-        is_listed = opts.listed,
-        output_qf = opts.output_qf,
-        output_lqf = opts.output_lqf,
-        bufnr = api.nvim_get_current_buf(),
-        efm = opts.efm,
-        notify = opts.notify
+    local job_info = {
+      bufnr = api.nvim_get_current_buf(),
+      title = buf_title,
+      stdout = {},
+      stderr = {},
+      output = {},
+      running = true,
+      start_time = fn.strftime('%H:%M:%S'),
+      finish_time = "",
+      exit_code = nil,
     }
+    s_jobs[job_id] = vim.tbl_extend("keep", job_info, opts)
     if opts.output_qf then
         utils.set_qflist({
             "[firvish] Job Started at " .. s_jobs[job_id].start_time

plugin/firvish.lua
@@ -25,7 +25,7 @@ if g.firvish_interactive_window_height == nil then
 end
 
 if fn.executable("rg") == 1 then
-    function _G.firvish_run_rg(args, use_last_buffer, qf, loc)
+    function _G.firvish_run_rg(args, use_last_buffer, qf, loc, open)
         local command = {
             "rg", "--column", "--line-number", "--no-heading", "--vimgrep",
             "--color=never", "--smart-case", "--block-buffered"
@@ -40,14 +40,16 @@ if fn.executable("rg") == 1 then
             listed = true,
             efm = {"%f:%l:%c:%m"},
             output_qf = qf,
+            open_qf = open,
             output_lqf = loc,
-            is_background_job = qf or loc
+            open_lqf = open,
+            is_background_job = qf or loc,
         })
     end
 
     cmd [[command! -bang -complete=file -nargs=* Rg :lua _G.firvish_run_rg({<f-args>}, "<bang>" == "!")]]
-    cmd [[command! -complete=file -nargs=* Crg :lua _G.firvish_run_rg({<f-args>}, false, true, false)]]
-    cmd [[command! -complete=file -nargs=* Lrg :lua _G.firvish_run_rg({<f-args>}, false, false, true)]]
+    cmd [[command! -bang -complete=file -nargs=* Crg :lua _G.firvish_run_rg({<f-args>}, false, true, false, "<bang>" == "!")]]
+    cmd [[command! -bang -complete=file -nargs=* Lrg :lua _G.firvish_run_rg({<f-args>}, false, false, true, "<bang>" == "!")]]
 end
 
 if fn.executable("ugrep") == 1 then

commit 374e19d7472d48e68f9c4b599674eba74f5ec6b7

fix: is_listed -> listed
broken by https://github.com/Furkanzmc/firvish.nvim/commit/5cbe8a72c38fc93963d662451fd45e87f93a33e9

lua/firvish/job_control.lua
@@ -166,7 +166,7 @@ M.delete_job_from_history = function(stop_job)
     end
 
     if job_info.running then
-        job_info.is_listed = false
+        job_info.listed = false
         fn.jobstop(info.job_id)
     else
         s_jobs[info.job_id] = nil
@@ -281,7 +281,7 @@ local function on_exit(job_id, exit_code, event)
         })
     end
 
-    if job_info.is_listed == true then
+    if job_info.listed == true then
         job_info.running = false
         job_info.exit_code = exit_code
         M.refresh_job_list_window()