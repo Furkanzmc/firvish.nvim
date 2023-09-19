7f4d115d065a22d257a163a48bbd40d572cf0c81
Furkan Uzumcu <furkanuzumcu@gmail.com>
2023-09-19 14:54:18 -0400

    Use vim.notify

--- lua/firvish/log.lua
+++ lua/firvish/log.lua
@@ -2,15 +2,11 @@ local vim = vim
 local log = {}
 
 function log.error(msg)
-    vim.api.nvim_command([[echohl ErrorMsg]])
-    vim.api.nvim_command("echo '[firvish] " .. msg .. "'")
-    vim.api.nvim_command([[echohl Normal]])
+    vim.notify("[firvish] " .. msg, vim.log.levels.ERROR, {})
 end
 
 function log.warning(msg)
-    vim.api.nvim_command([[echohl WarningMsg]])
-    vim.api.nvim_command("echo '[firvish] " .. msg .. "'")
-    vim.api.nvim_command([[echohl Normal]])
+    vim.notify("[firvish] " .. msg, vim.log.levels.WARN, {})
 end
 
 return log
